6084b4709956518879c557bb4f80a4ad558e1c04
Maciej Procyk <maciej.procyk@jetbrains.com>
2025-10-23 01:42:39 +0200

    Revert "migrate to `com.vanniktech.maven.publish`"
    This reverts commit 8bd4a66c32930573f710cea70e134f676853725c.

--- convention-plugins/build.gradle.kts
+++ convention-plugins/build.gradle.kts
@@ -2,11 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-dependencies {
-    implementation("com.vanniktech.maven.publish:com.vanniktech.maven.publish.gradle.plugin:0.34.0")
-}
-
 repositories {
     gradlePluginPortal()
-    mavenCentral()
 }
--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -1,43 +1,92 @@
+import java.util.*
+
 plugins {
-    id("com.vanniktech.maven.publish")
+    id("maven-publish") apply true
+    id("signing") apply true
 }
 
+ext["signing.keyId"] = null
+ext["signing.password"] = null
+ext["signing.key"] = null
+ext["ossrhUsername"] = null
+ext["ossrhPassword"] = null
+
+val secretPropsFile = project.rootProject.file("local.properties")
+if (secretPropsFile.exists()) {
+    secretPropsFile.reader().use {
+        Properties().apply { load(it) }
+    }.onEach { (name, value) ->
+        ext[name.toString()] = value
+    }
+} else {
+    ext["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
+    ext["signing.password"] = System.getenv("SIGNING_PASSWORD")
+    ext["signing.key"] = System.getenv("SIGNING_KEY")
+    ext["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
+    ext["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+}
+
+operator fun ExtraPropertiesExtension.invoke(name: String) = ext[name]?.toString()
+
 afterEvaluate {
-    mavenPublishing {
-        configureBasedOnAppliedPlugins(sourcesJar = true, javadocJar = false)
-
-        publishToMavenCentral()
-
-        signAllPublications()
-
-        pom {
-            val githubUrl = "https://github.com/avan1235/compose-extensions"
-
-            name = "Compose Multiplatform Extensions"
-            description = "Helper functions and extensions when working with compose-multiplatform projects"
-            url = githubUrl
-
-            licenses {
-                license {
-                    name = "MIT"
-                    url = "https://opensource.org/licenses/MIT"
+    publishing {
+        repositories {
+            maven {
+                name = "sonatype"
+                setUrl("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
+                credentials {
+                    username = ext("ossrhUsername")
+                    password = ext("ossrhPassword")
                 }
             }
-            developers {
-                developer {
-                    id = "avan1235"
-                    name = "Maciej Procyk"
-                    email = "maciej@procyk.in"
-                    url = "https://procyk.in"
-                }
+            mavenLocal()
+        }
+
+        publications.withType<MavenPublication> {
+            val publication = this
+            val javadocJar = tasks.register("${publication.name}JavadocJar", Jar::class) {
+                archiveClassifier = "javadoc"
+                archiveBaseName = "${archiveBaseName.get()}-${publication.name}"
             }
-            issueManagement {
-                system = "GitHub"
-                url = "$githubUrl/issues"
-            }
-            scm {
+            artifact(javadocJar)
+            pom {
+                val githubUrl = "https://github.com/avan1235/compose-extensions"
+
+                name = "Compose Multiplatform Extensions"
+                description = "Helper functions and extensions when working with compose-multiplatform projects"
                 url = githubUrl
+
+                licenses {
+                    license {
+                        name = "MIT"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "avan1235"
+                        name = "Maciej Procyk"
+                        email = "maciej@procyk.in"
+                        url = "https://procyk.in"
+                    }
+                }
+                issueManagement {
+                    system = "GitHub"
+                    url = "$githubUrl/issues"
+                }
+                scm {
+                    url = githubUrl
+                }
             }
         }
+    }
+
+    signing {
+        useInMemoryPgpKeys(
+            ext("signing.keyId"),
+            ext("signing.key"),
+            ext("signing.password"),
+        )
+        sign(publishing.publications)
     }
 }
