8bd4a66c32930573f710cea70e134f676853725c
Maciej Procyk <maciej.procyk@jetbrains.com>
2025-10-23 01:29:22 +0200

    migrate to `com.vanniktech.maven.publish`

--- convention-plugins/build.gradle.kts
+++ convention-plugins/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     `kotlin-dsl`
 }
 
+dependencies {
+    implementation("com.vanniktech.maven.publish:com.vanniktech.maven.publish.gradle.plugin:0.34.0")
+}
+
 repositories {
     gradlePluginPortal()
+    mavenCentral()
 }
